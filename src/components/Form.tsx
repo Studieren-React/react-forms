e66a084cd9523e092beb49901832aa6d570a0701
Verda Muso <gearmobile@gmail.com>
2024-04-23 21:40:21 +0300

    add select element

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -8,10 +8,11 @@ export class Form extends Component {
     state = {
         userName: '',
         userEmail: '',
-        userMessage: ''
+        userMessage: '',
+        userSelect: undefined
     };
 
-    handleChange = (event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
+    handleChange = (event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>): void => {
         this.setState({[event.currentTarget.name]: event.currentTarget.value})
     }
 
@@ -32,7 +33,7 @@ export class Form extends Component {
     }
 
     render() {
-        const { userName, userEmail, userMessage } = this.state;
+        const { userName, userEmail, userMessage, userSelect } = this.state;
 
         return (
             <div className="Form">
@@ -61,6 +62,16 @@ export class Form extends Component {
                     value={userMessage}
                     onChange={this.handleChange}
                 />
+                <br />
+                <select
+                    name="userSelect"
+                    value={userSelect}
+                    onChange={this.handleChange}
+                >
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                </select>
             </div>
         )
     }
